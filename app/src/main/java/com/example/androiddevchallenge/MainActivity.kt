857d947528f007c27572bde71a572a2f0dad1589
Patxi Bocos <patxi952@gmail.com>
2021-02-25 17:50:55 +0100

    Linter fix

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -24,12 +24,21 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -125,9 +134,12 @@ fun CatsList(cats: List<Cat>, catClicked: (Cat) -> Unit) {
         cells = GridCells.Fixed(2),
         contentPadding = PaddingValues(10.dp)
     ) {
-        items(cats, itemContent = { item ->
-            CatItem(cat = item, catClicked)
-        })
+        items(
+            cats,
+            itemContent = { item ->
+                CatItem(cat = item, catClicked)
+            }
+        )
     }
 }
 
